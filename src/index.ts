f2f2a06096c48857e71b7e62aa98c000a71a2e16
JR <kelly.mulumbi@strathmore.edu>
2021-08-12 10:39:26 +0300

    feat: Initial graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
-import { __prod__ } from "./constants";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
 import "reflect-metadata";
+import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import ormconfig from "./ormconfig";
+import { HelloResolver } from "./resolvers/hello";
 
-createConnection(ormconfig)
-  .then(() => {
-    // here you can start to work with your entities
-  })
-  .catch((error) => console.log(error));
+const main = async () => {
+  // Creates database connection
+  createConnection(ormconfig)
+    .then(() => {
+      // here you can start to work with your entities
+    })
+    .catch((error) => console.log(error));
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("%cServer started on localhost:4000", "color: green");
+  });
+};
+
+main();
