a78cf05ed80e8485911b776ae51c406dec037569
JR <kelly.mulumbi@strathmore.edu>
2021-08-12 23:46:26 +0300

    deletes editor hidden files

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_SESSION_EXPIRY = exports.__prod__ = void 0;
-exports.__prod__ = process.env.environment === "production";
-exports.DEFAULT_SESSION_EXPIRY = 1 * 60 * 60 * 1000;
-//# sourceMappingURL=constants.js.map
