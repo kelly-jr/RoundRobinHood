28934c5cf195f17e9d03f59840078e24170e7198
JR <kelly.mulumbi@strathmore.edu>
2021-08-19 00:11:45 +0300

    Auto deletes expired session by running migration

--- migration/1629319855031-DeleteSession.ts
+++ migration/1629319855031-DeleteSession.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class DeleteSession1629319855031 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`delete from "session" where "session"."sessionExpiry" < now() - interval '3 days'`);
+    await queryRunner.query(`delete from "session" where "session"."sessionExpiry" < now()-'2 hour'::interval`);
   }
 
   public async down(): Promise<void> {}
