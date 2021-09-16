77de6e85d6250b016f7ff4b8a866a37b521652cf
Okwonks <albertoketch@gmail.com>
2021-09-16 18:40:33 +0300

    user: add some CRUD options

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,6 +1,12 @@
-import { User } from "../entities";
-import { Arg, Ctx, Query, Resolver } from "type-graphql";
+import { User, Session } from "../entities";
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "src/types";
+
+@InputType()
+class UserInput {
+	@Field()
+	name: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -23,4 +29,36 @@ export class UserResolver {
       .where({ id });
     return userTable.getOne();
   }
+
+	// Create
+	@Mutation(() => User)
+	createUser(@Arg("options") options: UserInput, @Ctx() { orm }: MyContext): Promise<User> {
+		const manager = orm.manager;
+
+		const user = manager.create(User, options);
+		return manager.save(user);
+	}
+	
+	// Update
+	@Mutation(() => User)
+	updateUser(@Arg("id") id: string, @Arg("name") name: string, @Ctx() { orm }: MyContext) {
+		const manager = orm.manager;
+		return manager
+			.update(User, { id }, { name })
+			.then(() => manager.findOne(User, id));
+	}
+
+	// Join Session
+	@Mutation(() => User)
+	async joinSession(@Arg("joiningCode") joiningCode: string, @Arg("name") username: string, @Ctx() { orm }: MyContext): Promise<User | undefined> {
+		const manager = orm.manager;
+
+		const session = await manager.findOneOrFail(Session, { joiningCode });
+		const user = await manager.findOneOrFail(User, { name: username });
+		session.users = [ user ];
+		
+		return manager
+			.save(session)
+			.then(() => manager.findOne(User, { name: username }));
+	}
 }
